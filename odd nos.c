cef676b035429d691bc4e286066a6d6e841cf711
tarzan911 <vaishnavmadassery@gmail.com>
2024-10-20 15:31:04 +0530

    Add files via upload

---
+++ odd nos.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("enter a value : ");
+	scanf("%d",&n);
+	i=1;
+	while(i<=n*2)
+	{
+		if(i%2==1)
+		{
+			printf("%d\t",i);
+		}
+	    i++;
+	}	
+}

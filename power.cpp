76569045db78de83e9d1384d93f174e45202df68
tarzan911 <vaishnavmadassery@gmail.com>
2024-10-20 15:04:35 +0530

    Add files via upload

---
+++ power.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter a value : ");
+	scanf("%d",&a);
+	printf("\n %d^1=%d",a,a);
+	printf("\n %d^2=%d",a,a*a);
+	printf("\n %d^3=%d",a,a*a*a);
+	printf("\n %d^4=%d",a,a*a*a*a);
+	printf("\n %d^5=%d",a,a*a*a*a*a);
+	return 0;
+}
